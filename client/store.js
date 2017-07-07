b1dafbb550b5bded87f7b1c81d8d930e7312d000
Student <jeff.kandel@gmail.com>
2017-07-07 17:15:03 -0400

    feat: create redux store

--- client/store.js
+++ client/store.js
@@ -1,3 +1,9 @@
+import {createStore} from 'redux'
+
+const initialState = {
+  messages: [],
+}
+
 /* -----------------    ACTION TYPES     ------------------ */
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
 
@@ -13,3 +19,16 @@ export const gotMessagesFromServer = (messages) => ({
 
 
 /* ------------       REDUCERS     ------------------ */
+function reducer(prevState = initialState, action) {
+  const newState = Object.assign({}, prevState)
+  switch (action.type) {
+    case GOT_MESSAGES_FROM_SERVER:
+      newState.messages = action.messages
+      break;
+  return newState
+  }
+
+  return newState;
+}
+
+export default createStore(reducer)
